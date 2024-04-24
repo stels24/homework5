520127153f59a91bf86db89406fc2443126972ce
stels24 <166283589+stels24@users.noreply.github.com>
2024-04-24 09:33:58 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ["apple", "strawberry", "orange", "grape", "plum", "gooseberry"]
+print(my_list)
+print(my_list [0::5])
+print(my_list [2:5])
+my_list [2] = "coconut"
+print(my_list)
+
+
+my_dict = {"apple": "яблоко", "strawberry": "клубника", "orange": "апельсин", "grape": "виноград", "plum": "слива"}
+print(my_dict ["strawberry"])
+my_dict["strawberry"] = "ягода"
+print(my_dict)
+
+
